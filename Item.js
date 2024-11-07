d565943305457b96c21753527e5ebd027c9dac40
kevincyubahiro <kevincyubahiro250@gmail.com>
2024-11-07 06:03:53 -0800

    node js crud

--- Item.js
+++ Item.js
@@ -17,3 +17,21 @@ const itemSchema = new mongoose.Schema({
 const Item = mongoose.model('Item', itemSchema);
 
 module.exports = Item;
+app.post('/items', (req, res) => {
+    const { name, description, price } = req.body;
+  
+    const newItem = new Item({
+      name,
+      description,
+      price
+    });
+  
+    newItem.save()
+      .then(item => {
+        res.status(201).json(item);
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  
